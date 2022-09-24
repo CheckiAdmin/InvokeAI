06ecfe2ea7fb1d521fe4b0371bd47953fd0850cb
myname <me@me>
2022-09-24 20:54:34 +1000

    commit everythng working atm.. lol committed dll and exe

--- scripts/txt2imghd.py
+++ scripts/txt2imghd.py
@@ -25,6 +25,7 @@ def chunk(it, size):
     it = iter(it)
     return iter(lambda: tuple(islice(it, size)), ())
 
+
 def numpy_to_pil(images):
     """
     Convert a numpy image or a batch of images to a PIL image.
@@ -35,6 +36,7 @@ def numpy_to_pil(images):
     pil_images = [Image.fromarray(image) for image in images]
 
     return pil_images
+
 
 def load_model_from_config(config, ckpt, verbose=False):
     print(f"Loading model from {ckpt}")
@@ -55,6 +57,7 @@ def load_model_from_config(config, ckpt, verbose=False):
     model.eval()
     return model
 
+
 def load_img(path):
     image = Image.open(path).convert("RGB")
     w, h = image.size
@@ -64,7 +67,8 @@ def load_img(path):
     image = np.array(image).astype(np.float32) / 255.0
     image = image[None].transpose(0, 3, 1, 2)
     image = torch.from_numpy(image)
-    return 2.*image - 1.
+    return 2.0 * image - 1.0
+
 
 def convert_pil_img(image):
     w, h = image.size
@@ -73,24 +77,29 @@ def convert_pil_img(image):
     image = np.array(image).astype(np.float32) / 255.0
     image = image[None].transpose(0, 3, 1, 2)
     image = torch.from_numpy(image)
-    return 2.*image - 1.
+    return 2.0 * image - 1.0
+
 
 def addalpha(im, mask):
     imr, img, imb, ima = im.split()
     mmr, mmg, mmb, mma = mask.split()
-    im = Image.merge('RGBA', [imr, img, imb, mma])  # we want the RGB from the original, but the transparency from the mask
-    return(im)
+    im = Image.merge(
+        "RGBA", [imr, img, imb, mma]
+    )  # we want the RGB from the original, but the transparency from the mask
+    return im
+
 
 # Alternative method composites a grid of images at the positions provided
 def grid_merge(source, slices):
     source.convert("RGBA")
-    for slice, posx, posy in slices: # go in reverse to get proper stacking
+    for slice, posx, posy in slices:  # go in reverse to get proper stacking
         source.alpha_composite(slice, (posx, posy))
     return source
 
+
 def grid_coords(target, original, overlap):
-    #generate a list of coordinate tuples for our sections, in order of how they'll be rendered
-    #target should be the size for the gobig result, original is the size of each chunk being rendered
+    # generate a list of coordinate tuples for our sections, in order of how they'll be rendered
+    # target should be the size for the gobig result, original is the size of each chunk being rendered
     center = []
     target_x, target_y = target
     center_x = int(target_x / 2)
@@ -98,19 +107,19 @@ def grid_coords(target, original, overlap):
     original_x, original_y = original
     x = center_x - int(original_x / 2)
     y = center_y - int(original_y / 2)
-    center.append((x,y)) #center chunk
-    uy = y #up
+    center.append((x, y))  # center chunk
+    uy = y  # up
     uy_list = []
-    dy = y #down
+    dy = y  # down
     dy_list = []
-    lx = x #left
+    lx = x  # left
     lx_list = []
-    rx = x #right
+    rx = x  # right
     rx_list = []
-    while uy > 0: #center row vertical up
+    while uy > 0:  # center row vertical up
         uy = uy - original_y + overlap
         uy_list.append((lx, uy))
-    while (dy + original_y) <= target_y: #center row vertical down
+    while (dy + original_y) <= target_y:  # center row vertical down
         dy = dy + original_y - overlap
         dy_list.append((rx, dy))
     while lx > 0:
@@ -137,8 +146,8 @@ def grid_coords(target, original, overlap):
             dy_list.append((rx, dy))
     # calculate a new size that will fill the canvas, which will be optionally used in grid_slice and go_big
     last_coordx, last_coordy = dy_list[-1:][0]
-    render_edgey = last_coordy + original_y # outer bottom edge of the render canvas
-    render_edgex = last_coordx + original_x # outer side edge of the render canvas
+    render_edgey = last_coordy + original_y  # outer bottom edge of the render canvas
+    render_edgex = last_coordx + original_x  # outer side edge of the render canvas
     scalarx = render_edgex / target_x
     scalary = render_edgey / target_y
     if scalarx <= scalary:
@@ -160,10 +169,12 @@ def grid_coords(target, original, overlap):
     result.append(center[0])
     return result, (new_edgex, new_edgey)
 
+
 def get_resampling_mode():
     try:
         from PIL import __version__, Image
-        major_ver = int(__version__.split('.')[0])
+
+        major_ver = int(__version__.split(".")[0])
         if major_ver >= 9:
             return Image.Resampling.LANCZOS
         else:
@@ -171,21 +182,47 @@ def get_resampling_mode():
     except Exception as ex:
         return 1  # 'Lanczos' irrespective of version.
 
+
 # Chop our source into a grid of images that each equal the size of the original render
-def grid_slice(source, overlap, og_size, maximize=False,):
-    width, height = og_size # size of the slices to be rendered
+def grid_slice(
+    source: Image,
+    overlap,
+    og_size,
+    maximize=False,
+):
+    width, height = og_size  # size of the slices to be rendered
     coordinates, new_size = grid_coords(source.size, og_size, overlap)
     if maximize == True:
-        source = source.resize(new_size, get_resampling_mode()) # minor concern that we're resizing twice
-        coordinates, new_size = grid_coords(source.size, og_size, overlap) # re-do the coordinates with the new canvas size
+        source = source.resize(
+            new_size, get_resampling_mode()
+        )  # minor concern that we're resizing twice
+        coordinates, new_size = grid_coords(
+            source.size, og_size, overlap
+        )  # re-do the coordinates with the new canvas size
     # loc_width and loc_height are the center point of the goal size, and we'll start there and work our way out
     slices = []
     for coordinate in coordinates:
         x, y = coordinate
-        slices.append(((source.crop((x, y, x+width, y+height))), x, y))
+        slices.append(
+            (
+                (
+                    source.crop(
+                        (
+                            x,
+                            y,
+                            x + width,
+                            y + height,
+                        ),
+                    ),
+                ),
+                x,
+                y,
+            ),
+        )
     global slices_todo
     slices_todo = len(slices) - 1
     return slices, new_size
+
 
 class Options:
     prompt: List[str]
@@ -210,33 +247,29 @@ class Options:
     generated: Optional[List[str]]
     img: str
 
+
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument(
-        "--prompt",
-        type=str,
-        nargs="?",
-        help="the prompt to render"
-    )
+    parser.add_argument("--prompt", type=str, nargs="?", help="the prompt to render")
     parser.add_argument(
         "--generated",
         type=str,
         nargs="?",
-        help="only do detailing, using these base filenames in output dir"
+        help="only do detailing, using these base filenames in output dir",
     )
     parser.add_argument(
         "--img",
         type=str,
         nargs="?",
-        help="only do detailing, using this path (will be copied to output dir)"
+        help="only do detailing, using this path (will be copied to output dir)",
     )
     parser.add_argument(
         "--outdir",
         type=str,
         nargs="?",
         help="dir to write results to",
-        default="outputs/txt2imghd-samples"
+        default="outputs/txt2imghd-samples",
     )
     parser.add_argument(
         "--steps",
@@ -246,7 +279,7 @@ def main():
     )
     parser.add_argument(
         "--ddim",
-        action='store_true',
+        action="store_true",
         help="use ddim sampling",
     )
     parser.add_argument(
@@ -330,7 +363,7 @@ def main():
         "--realesrgan",
         type=str,
         default="realesrgan-ncnn-vulkan",
-        help="path to realesrgan executable"
+        help="path to realesrgan executable",
     )
     parser.add_argument(
         "--detail_steps",
@@ -356,21 +389,20 @@ def main():
         opt.prompt = input("prompt: ")
     text2img2(opt)
 
+
 def realesrgan2x(executable: str, input: str, output: str):
-    process = subprocess.Popen([
-        executable,
-        '-i',
-        input,
-        '-o',
-        output,
-        '-n',
-        'realesrgan-x4plus'
-    ])
+    process = subprocess.Popen(
+        [executable, "-i", input, "-o", output, "-n", "realesrgan-x4plus"]
+    )
     process.wait()
 
     final_output = Image.open(output)
-    final_output = final_output.resize((int(final_output.size[0] / 2), int(final_output.size[1] / 2)), get_resampling_mode())
+    final_output = final_output.resize(
+        (int(final_output.size[0] / 2), int(final_output.size[1] / 2)),
+        get_resampling_mode(),
+    )
     final_output.save(output)
+
 
 def text2img2(opt: Options):
 
@@ -430,28 +462,40 @@ def text2img2(opt: Options):
                                 prompts = list(prompts)
                             c = model.get_learned_conditioning(prompts)
                             shape = [opt.C, opt.H // opt.f, opt.W // opt.f]
-                            samples_ddim, _ = sampler.sample(S=opt.steps,
-                                                            conditioning=c,
-                                                            batch_size=batch_size,
-                                                            shape=shape,
-                                                            verbose=False,
-                                                            unconditional_guidance_scale=opt.scale,
-                                                            unconditional_conditioning=uc,
-                                                            eta=0,
-                                                            x_T=None)
+                            samples_ddim, _ = sampler.sample(
+                                S=opt.steps,
+                                conditioning=c,
+                                batch_size=batch_size,
+                                shape=shape,
+                                verbose=False,
+                                unconditional_guidance_scale=opt.scale,
+                                unconditional_conditioning=uc,
+                                eta=0,
+                                x_T=None,
+                            )
 
                             x_samples_ddim = model.decode_first_stage(samples_ddim)
-                            x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
-                            x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
+                            x_samples_ddim = torch.clamp(
+                                (x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0
+                            )
+                            x_samples_ddim = (
+                                x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
+                            )
 
                             x_checked_image = x_samples_ddim
 
-                            x_checked_image_torch = torch.from_numpy(x_checked_image).permute(0, 3, 1, 2)
+                            x_checked_image_torch = torch.from_numpy(
+                                x_checked_image
+                            ).permute(0, 3, 1, 2)
 
                             for x_sample in x_checked_image_torch:
-                                x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
+                                x_sample = 255.0 * rearrange(
+                                    x_sample.cpu().numpy(), "c h w -> h w c"
+                                )
                                 img = Image.fromarray(x_sample.astype(np.uint8))
-                                output_path = os.path.join(sample_path, f"{base_count:05}.png")
+                                output_path = os.path.join(
+                                    sample_path, f"{base_count:05}.png"
+                                )
                                 img.save(output_path)
                                 generated.append(f"{base_count:05}")
                                 base_count += 1
@@ -463,23 +507,33 @@ def text2img2(opt: Options):
 
     for base_filename in generated:
         for _ in trange(opt.passes, desc="Passes"):
-            realesrgan2x(opt.realesrgan, os.path.join(sample_path, f"{base_filename}.png"), os.path.join(sample_path, f"{base_filename}u.png"))
+            realesrgan2x(
+                opt.realesrgan,
+                os.path.join(sample_path, f"{base_filename}.png"),
+                os.path.join(sample_path, f"{base_filename}u.png"),
+            )
             base_filename = f"{base_filename}u"
 
             source_image = Image.open(os.path.join(sample_path, f"{base_filename}.png"))
-            og_size = (opt.H,opt.W)
+            og_size = (opt.H, opt.W)
             slices, _ = grid_slice(source_image, opt.gobig_overlap, og_size, False)
 
             betterslices = []
             for _, chunk_w_coords in tqdm(enumerate(slices), "Slices"):
                 chunk, coord_x, coord_y = chunk_w_coords
                 init_image = convert_pil_img(chunk).to(device)
-                init_image = repeat(init_image, '1 ... -> b ...', b=batch_size)
-                init_latent = model.get_first_stage_encoding(model.encode_first_stage(init_image))  # move to latent space
+                init_image = repeat(init_image, "1 ... -> b ...", b=batch_size)
+                init_latent = model.get_first_stage_encoding(
+                    model.encode_first_stage(init_image)
+                )  # move to latent space
 
-                sampler.make_schedule(ddim_num_steps=opt.detail_steps, ddim_eta=0, verbose=False)
+                sampler.make_schedule(
+                    ddim_num_steps=opt.detail_steps, ddim_eta=0, verbose=False
+                )
 
-                assert 0. <= opt.strength <= 1., 'can only work with strength in [0.0, 1.0]'
+                assert (
+                    0.0 <= opt.strength <= 1.0
+                ), "can only work with strength in [0.0, 1.0]"
                 t_enc = int(opt.strength * opt.detail_steps)
 
                 with torch.inference_mode():
@@ -488,44 +542,64 @@ def text2img2(opt: Options):
                             for prompts in tqdm(data, desc="data"):
                                 uc = None
                                 if opt.detail_scale != 1.0:
-                                    uc = model.get_learned_conditioning(batch_size * [""])
+                                    uc = model.get_learned_conditioning(
+                                        batch_size * [""]
+                                    )
                                 if isinstance(prompts, tuple):
                                     prompts = list(prompts)
                                 c = model.get_learned_conditioning(prompts)
 
                                 # encode (scaled latent)
-                                z_enc = sampler.stochastic_encode(init_latent, torch.tensor([t_enc]*batch_size).to(device))
+                                z_enc = sampler.stochastic_encode(
+                                    init_latent,
+                                    torch.tensor([t_enc] * batch_size).to(device),
+                                )
                                 # decode it
-                                samples = sampler.decode(z_enc, c, t_enc, unconditional_guidance_scale=opt.detail_scale,
-                                                        unconditional_conditioning=uc,)
+                                samples = sampler.decode(
+                                    z_enc,
+                                    c,
+                                    t_enc,
+                                    unconditional_guidance_scale=opt.detail_scale,
+                                    unconditional_conditioning=uc,
+                                )
 
                                 x_samples = model.decode_first_stage(samples)
-                                x_samples = torch.clamp((x_samples + 1.0) / 2.0, min=0.0, max=1.0)
+                                x_samples = torch.clamp(
+                                    (x_samples + 1.0) / 2.0, min=0.0, max=1.0
+                                )
 
                                 for x_sample in x_samples:
-                                    x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
-                                    resultslice = Image.fromarray(x_sample.astype(np.uint8)).convert('RGBA')
-                                    betterslices.append((resultslice.copy(), coord_x, coord_y))
+                                    x_sample = 255.0 * rearrange(
+                                        x_sample.cpu().numpy(), "c h w -> h w c"
+                                    )
+                                    resultslice = Image.fromarray(
+                                        x_sample.astype(np.uint8)
+                                    ).convert("RGBA")
+                                    betterslices.append(
+                                        (resultslice.copy(), coord_x, coord_y)
+                                    )
 
-            alpha = Image.new('L', og_size, color=0xFF)
+            alpha = Image.new("L", og_size, color=0xFF)
             alpha_gradient = ImageDraw.Draw(alpha)
             a = 0
             i = 0
             overlap = opt.gobig_overlap
-            shape = (og_size, (0,0))
+            shape = (og_size, (0, 0))
             while i < overlap:
-                alpha_gradient.rectangle(shape, fill = a)
+                alpha_gradient.rectangle(shape, fill=a)
                 a += 4
                 i += 1
-                shape = ((og_size[0] - i, og_size[1]- i), (i,i))
-            mask = Image.new('RGBA', og_size, color=0)
+                shape = ((og_size[0] - i, og_size[1] - i), (i, i))
+            mask = Image.new("RGBA", og_size, color=0)
             mask.putalpha(alpha)
             finished_slices = []
             for betterslice, x, y in betterslices:
                 finished_slice = addalpha(betterslice, mask)
                 finished_slices.append((finished_slice, x, y))
             # # Once we have all our images, use grid_merge back onto the source, then save
-            final_output = grid_merge(source_image.convert("RGBA"), finished_slices).convert("RGB")
+            final_output = grid_merge(
+                source_image.convert("RGBA"), finished_slices
+            ).convert("RGB")
             final_output.save(os.path.join(sample_path, f"{base_filename}d.png"))
             base_filename = f"{base_filename}d"
 
@@ -533,6 +607,7 @@ def text2img2(opt: Options):
             gc.collect()
 
         final_output.save(os.path.join(sample_path, f"{base_filename}.png"))
+
 
 if __name__ == "__main__":
     main()
